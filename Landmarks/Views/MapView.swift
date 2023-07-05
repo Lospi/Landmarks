3dfbeef1416d0d2ef12ff1280dd83d37b17fbddf
Roberto <roberto3154_421@hotmail.com>
2023-07-04 23:02:15 -0300

    feat: pass data per child

--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -9,15 +9,23 @@ import MapKit
 import SwiftUI
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
+    var coordinate: CLLocationCoordinate2D
+    @State private var region = MKCoordinateRegion()
     var body: some View {
         Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
+            }
+    }
+
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
     }
 }
 
 #Preview {
-    MapView()
+    MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868))
 }
